b704c15c83b872285ce86f14d95cc5cb87fbc5ed
Milan Lesichkov <lesichkovm@gmail.com>
2020-07-18 11:27:03 +0100

    Update tag.go

--- tag.go
+++ tag.go
@@ -18,12 +18,37 @@ func addslashes(str string) string {
 	return buf.String()
 }
 
+func inArrayString(arr []string, str string) bool {
+	for _, a := range arr {
+		if a == str {
+			return true
+		}
+	}
+	return false
+}
+
+// NewButton represents a BUTTON tag
+func NewButton() *Tag {
+	tag := &Tag{
+		TagName: "button",
+	}
+	return tag
+}
+
 // NewDiv represents a DIV tag
 func NewDiv() *Tag {
-	section := &Tag{
+	tag := &Tag{
 		TagName: "div",
 	}
-	return section
+	return tag
+}
+
+// NewForm represents a IMG tag
+func NewForm() *Tag {
+	tag := &Tag{
+		TagName: "input",
+	}
+	return tag
 }
 
 // NewHTML creates pure HTML without surrounding tags
@@ -33,6 +58,86 @@ func NewHTML(html string) *Tag {
 		TagContent: html,
 	}
 	return textTag
+}
+
+// NewHeading1 represents a H1 tag
+func NewHeading1() *Tag {
+	tag := &Tag{
+		TagName: "h1",
+	}
+	return tag
+}
+
+// NewHeading2 represents a H1 tag
+func NewHeading2() *Tag {
+	tag := &Tag{
+		TagName: "h2",
+	}
+	return tag
+}
+
+// NewHeading3 represents a H1 tag
+func NewHeading3() *Tag {
+	tag := &Tag{
+		TagName: "h3",
+	}
+	return tag
+}
+
+// NewHeading4 represents a H1 tag
+func NewHeading4() *Tag {
+	tag := &Tag{
+		TagName: "h4",
+	}
+	return tag
+}
+
+// NewHeading5 represents a H1 tag
+func NewHeading5() *Tag {
+	tag := &Tag{
+		TagName: "h5",
+	}
+	return tag
+}
+
+// NewHeading6 represents a H1 tag
+func NewHeading6() *Tag {
+	tag := &Tag{
+		TagName: "h6",
+	}
+	return tag
+}
+
+// NewHyperlink represents a H1 tag
+func NewHyperlink() *Tag {
+	tag := &Tag{
+		TagName: "a",
+	}
+	return tag
+}
+
+// NewImage represents a IMG tag
+func NewImage() *Tag {
+	tag := &Tag{
+		TagName: "img",
+	}
+	return tag
+}
+
+// NewInput represents a IMG tag
+func NewInput() *Tag {
+	tag := &Tag{
+		TagName: "input",
+	}
+	return tag
+}
+
+// NewParagraph represents a IMG tag
+func NewParagraph() *Tag {
+	tag := &Tag{
+		TagName: "p",
+	}
+	return tag
 }
 
 // NewScript represents a STYLE tag
@@ -46,10 +151,18 @@ func NewScript(javascript string) *Tag {
 
 // NewSection represents a SECTION tag
 func NewSection() *Tag {
-	section := &Tag{
+	tag := &Tag{
 		TagName: "section",
 	}
-	return section
+	return tag
+}
+
+// NewSpan represents a SPAN tag
+func NewSpan() *Tag {
+	tag := &Tag{
+		TagName: "span",
+	}
+	return tag
 }
 
 // NewStyle represents a STYLE tag
@@ -107,10 +220,21 @@ func (t *Tag) GetAttribute(key string) string {
 
 // ToHTML returns HTML from Node
 func (t *Tag) ToHTML() string {
+	shortTags := []string{
+		"img",
+		"input",
+		"link",
+	}
+
+	isShortTag := inArrayString(shortTags, t.TagName)
+
 	tagStart := `<` + t.TagName + t.attrToString() + `>`
 	tagEnd := `</` + t.TagName + `>`
 	if t.TagName == "" {
 		tagStart = ""
+		tagEnd = ""
+	}
+	if isShortTag {
 		tagEnd = ""
 	}
 	return tagStart + t.TagContent + t.childrenToString() + tagEnd
@@ -123,7 +247,7 @@ func (t Tag) attrToString() string {
 		if strings.Trim(value, " ") == "" {
 			continue
 		}
-		attrString += ` "` + key + `"="` + addslashes(value) + `"`
+		attrString += ` ` + key + `="` + addslashes(value) + `"`
 	}
 
 	if attrString != "" {
