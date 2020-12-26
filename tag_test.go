1a3d67b50b36f6c6452ee22d5eb5562a0f2e566f
Milan Lesichkov <lesichkovm@gmail.com>
2020-12-26 11:02:39 +0000

    Create tag_test.go

---
+++ tag_test.go
@@ -0,0 +1,17 @@
+package html
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestEscapeAttributes(t *testing.T) {
+    tag := &Tag{
+		TagName: "div",
+	}
+	tag.Attr("onclick","page('PAGE_ID')")
+	h := tag.ToHTML()
+	if strings.Contains(h, "onclick=\"page('PAGE_ID')\"") == false{
+		t.Error("Does not contain onclick=\"page('PAGE_ID')\"", "Output:" + h)
+	}
+}
