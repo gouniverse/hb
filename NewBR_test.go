c712d7fb3ffc7af7fc5691212963d864c1c82dcc
Milan Lesichkov <lesichkovm@gmail.com>
2022-09-15 20:31:57 +0100

    Update NewBR_test.go

--- NewBR_test.go
+++ NewBR_test.go
@@ -9,10 +9,7 @@ func TestNewBR(t *testing.T) {
 	br := NewBR()
 	html := br.ToHTML()
 
-	if strings.Contains(html, "<br>") == false {
-		t.Error("Does not contain '<br>', Output:" + html)
-	}
-	if strings.Contains(html, "</br>") == false {
-		t.Error("Does not contain '</br>', Output:" + html)
+	if strings.Contains(html, "<br />") == false {
+		t.Error("Does not contain '<br />', Output:" + html)
 	}
 }
