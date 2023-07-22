eb776a934e569a0d619cd55d4b9393aff7a3e7a2
Milan Lesichkov <lesichkovm@gmail.com>
2023-07-22 21:43:39 +0100

    Update Conditions.go

--- Conditions.go
+++ Conditions.go
@@ -7,9 +7,25 @@ func If(condition bool, trueTag *Tag) *Tag {
 	return nil
 }
 
+func (t *Tag) IfF(condition bool, trueFunc func() *Tag) *Tag {
+	if condition {
+		return trueFunc()
+	}
+
+	return nil
+}
+
 func IfElse(condition bool, trueTag *Tag, falseTag *Tag) *Tag {
 	if condition {
 		return trueTag
 	}
 	return falseTag
+}
+
+func (t *Tag) IfFElseF(condition bool, trueFunc func() *Tag, falseFunc func() *Tag) *Tag {
+	if condition {
+		return trueFunc()
+	}
+
+	return falseFunc()
 }
