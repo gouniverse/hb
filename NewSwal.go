865c2d0df60e600a28e4db52f682118c885d90be
Milan Lesichkov <lesichkovm@gmail.com>
2024-11-11 21:56:55 +0000

    Update NewSwal.go

--- NewSwal.go
+++ NewSwal.go
@@ -10,6 +10,7 @@ type SwalOptions struct {
 	Title              string `json:"title,omitempty"`
 	Position           string `json:"position,omitempty"`
 	Timer              int    `json:"timer,omitempty"`
+	TimerProgressBar   bool    `json:"timerProgressBar,omitempty"`
 	ShowCancelButton   bool   `json:"showCancelButton,omitempty"`
 	ShowConfirmButton  bool   `json:"showConfirmButton,omitempty"`
 	CancelButtonColor  string `json:"cancelButtonColor,omitempty"`
@@ -17,6 +18,15 @@ type SwalOptions struct {
 	ConfirmButtonText  string `json:"confirmButtonText,omitempty"`
 	ConfirmButtonColor string `json:"confirmButtonColor,omitempty"`
 	ConfirmCallback    string `json:"-"`
+	ImageURL	   string `json:"imageUrl,omitempty"`
+	ImageWidth	   string `json:"imageWidth,omitempty"`
+	ImageHeight	   string `json:"imageHeight,omitempty"`
+	ImageAlt	   string `json:"imageAlt,omitempty"`
+	Width		   string `json:"width,omitempty"`
+	Padding		   string `json:"padding,omitempty"`
+	Color		   string `json:"color,omitempty"`
+	Background	   string `json:"background,omitempty"`
+	Backdrop	   string `json:"backdrop,omitempty"`
 }
 
 // NewSwal generates a script with a Sweetalert2 dialog
